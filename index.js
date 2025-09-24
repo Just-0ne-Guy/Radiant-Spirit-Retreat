e6e48187e24cda0a4278e1dbdb5148fdf2d0aed6
Chris <cmatthew2402@gmail.com>
2025-09-24 18:05:00 -0400

    modal added and contact

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+let current = 0;
+let isModalOpen = false;
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const testimonials = document.querySelectorAll(".testimonial");
   const prevBtn = document.querySelectorAll(".testimonial__btn")[0];
   const nextBtn = document.querySelectorAll(".testimonial__btn")[1];
 
-  let current = 0;
 
   function showTestimonial(index) {
     testimonials.forEach((testimonial, i) => {
@@ -22,5 +25,40 @@ document.addEventListener("DOMContentLoaded", () => {
     showTestimonial(current);
   });
 
-  showTestimonial(current); // initialize first one
+  showTestimonial(current); 
 });
+
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visable";
+
+  emailjs
+    .sendForm(
+      "service_tslm6uj",
+      "template_qxbe627",
+      event.target,
+      "E892NPs1JL09A2C8y"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visable");
+      success.classList += " modal__overlay--visable";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visable");
+      alert(
+        "The email service is temporarly unavailable. Please contact me directly directly on Cmatthew2402@gmail.com"
+      );
+    });
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  } else isModalOpen = true;
+  document.body.classList += " modal--open";
+}
+ 
